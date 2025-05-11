8ee958bfbb836bfbcd46b01307acdc90293e499f
VALCAZA <victor.enrique.alcazar@lts-beratung.de>
2025-05-11 13:55:57 +0200

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  reactStrictMode: true,
+  // necessary so images work in static export:
+  images: { unoptimized: true },
+  // prepend repo name to all URLs when deployed:
+  assetPrefix: isProd ? '/cat-gardener/' : '',
+  basePath:    isProd ? '/cat-gardener'  : '',
+  // enable static HTML export:
+  output: 'export'
 };
 
 module.exports = nextConfig;
